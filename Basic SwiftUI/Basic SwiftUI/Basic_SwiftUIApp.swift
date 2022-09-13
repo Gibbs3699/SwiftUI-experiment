f3157be0bab86ff08eb28587083236a8bceb75ac
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-09-13 22:50:57 +0700

    init project

---
+++ Basic SwiftUI/Basic SwiftUI/Basic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_SwiftUIApp.swift
+//  Basic SwiftUI
+//
+//  Created by TheGIZzz on 13/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Basic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic SwiftUI/Basic SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Basic SwiftUI
+//
+//  Created by TheGIZzz on 13/9/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
