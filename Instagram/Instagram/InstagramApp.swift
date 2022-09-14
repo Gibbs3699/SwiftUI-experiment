351b29a48fb763589cbb42cce0ce3fb87eaa5058
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-09-14 22:49:26 +0700

    init instagram app

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by TheGIZzz on 14/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
