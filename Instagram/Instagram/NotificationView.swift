093c0365baeb11f50f425c743192a7a415f44caa
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-09-15 23:04:48 +0700

    feat: tab bar setting and info

---
+++ Instagram/Instagram/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Instagram
+//
+//  Created by TheGIZzz on 15/9/2565 BE.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
---
+++ Instagram/Instagram/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Instagram
+//
+//  Created by TheGIZzz on 15/9/2565 BE.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
