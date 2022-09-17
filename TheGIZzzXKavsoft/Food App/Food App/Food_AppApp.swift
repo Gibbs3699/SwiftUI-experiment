a97a81e0d494a5508a958b6103e67b7408c02dc5
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-09-17 22:33:46 +0700

    build: create food app

---
+++ TheGIZzzXKavsoft/Food App/Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by TheGIZzz on 17/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
