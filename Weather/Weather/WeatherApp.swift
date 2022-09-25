de999276d9b49e6b316b974da496532b16396a7e
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-09-25 22:04:10 +0700

    adding weather app

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by TheGIZzz on 25/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
