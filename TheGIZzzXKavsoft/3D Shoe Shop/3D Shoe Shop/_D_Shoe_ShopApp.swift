58c662a1dd57bacfbdf8fa3076f72bf19b26702f
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-10-13 23:03:40 +0700

    kavsoft: 3d shoe shop

---
+++ TheGIZzzXKavsoft/3D Shoe Shop/3D Shoe Shop/_D_Shoe_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_Shoe_ShopApp.swift
+//  3D Shoe Shop
+//
+//  Created by TheGIZzz on 13/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct _D_Shoe_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
