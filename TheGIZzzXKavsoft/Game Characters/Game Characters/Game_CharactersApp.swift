b252fc25eb2a256e0e7cb7b673d6a79418d9f3a7
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-05-28 16:59:04 +0700

    init Game Characters project

---
+++ TheGIZzzXKavsoft/Game Characters/Game Characters/Game_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CharactersApp.swift
+//  Game Characters
+//
+//  Created by TheGIZzz on 28/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Game_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
