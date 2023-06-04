5dc5caf4e7e969b68d7677bdfa1f5b3e47ff3845
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-06-04 21:01:00 +0700

    add CharactersDataService

---
+++ TheGIZzzXKavsoft/Game Characters/Game Characters/Services/CharactersDataService.swift
@@ -0,0 +1,20 @@
+//
+//  CharactersDataService.swift
+//  Game Characters
+//
+//  Created by TheGIZzz on 4/6/2566 BE.
+//
+
+import SwiftUI
+
+struct CharactersDataService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharactersDataService_Previews: PreviewProvider {
+    static var previews: some View {
+        CharactersDataService()
+    }
+}
